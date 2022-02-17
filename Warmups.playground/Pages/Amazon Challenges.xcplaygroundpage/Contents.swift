1e3fe2534bfed8c98f0cfb8d9c3cd4fda2d12ed8
SpencerNinja <62817123+SpencerNinja@users.noreply.github.com>
2022-02-17 14:09:41 -0700

    Amazon challenge

---
+++ Warmups.playground/Pages/Amazon Challenges.xcplaygroundpage/Contents.swift
@@ -0,0 +1,68 @@
+/*
+ Create a program that tells an Amazon Delivery Driver how to pack variable number of trucks evenly with a product. The products will be represented by an array of weights.
+ */
+
+// We have a pile of packages with different weights
+// We have a number of trucks waiting to load said packages
+// Find out the total weight of the packages
+// Determine how to evenly distribute the weight between trucks
+// Sort packages by heaviest first
+// Start adding packages to the 1st truck until it is full
+// Move onto next truck and repeat
+// Return a list of weights for each truck
+
+import Foundation
+
+func calcMinimumNumPackages(numTrucks: Int, weightsArray: [Package]) -> [[Int]] {
+    let sum = weightsArray.map { $0.weight }.reduce(0, +)
+    let avg = sum / numTrucks
+    var allWeights = weightsArray
+    for _ in 1...numTrucks {
+        trucks.append(Truck(maxWeight: avg))
+    }
+    for truck in trucks {
+        truck.fill(amount: avg, packages: allWeights)
+    }
+    
+    return [[]]
+}
+
+struct Truck {
+    let maxWeight: Int
+    var payload: Int
+    
+    mutating func fill(amount: Int, packages: [Package]) {
+        // Loop through packages and fill the truck with any that will fit
+        for package in packages {
+            // check if the package will fit in the truck
+            if package.weight < self.maxWeight {
+                payload += package.weigth
+                AmazonManager.packages.removeAll(where: package.id == )
+            }
+        }
+    }
+}
+
+struct Package: Identifiable {
+    let id = UUID().uuidString
+    let weight: Int
+}
+
+class AmazonManager {
+    
+}
+static var packages = [
+    Package(weight: 1),
+    Package(weight: 2),
+    Package(weight: 3),
+    Package(weight: 3),
+    Package(weight: 4),
+    Package(weight: 4),
+    Package(weight: 4),
+    Package(weight: 5),
+    Package(weight: 6),
+    Package(weight: 7),
+    Package(weight: 7),
+    Package(weight: 9)
+]
+print(calcMinimumNumPackages(numTrucks: 3, weights: weights.sorted(by: { $0 > $1 })))
